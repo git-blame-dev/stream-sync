919c49d7e3010e17aa27e350e1b473e7763de26c
git-blame-dev <251953086+git-blame-dev@users.noreply.github.com>
2026-01-29 16:31:30 +1100

    feat(auth): add TwitchAuth and OAuth scopes

---
+++ src/auth/twitch-oauth-scopes.js
@@ -0,0 +1,12 @@
+const TWITCH_OAUTH_SCOPES = [
+    'user:read:chat',
+    'chat:edit',
+    'channel:read:subscriptions',
+    'bits:read',
+    'channel:read:redemptions',
+    'moderator:read:followers'
+];
+
+module.exports = {
+    TWITCH_OAUTH_SCOPES
+};
